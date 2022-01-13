1d9b7d2cd68fd3821d81edde37ff4534f8c1011c
omari <omari.jalagania@gmail.com>
2022-01-13 19:25:38 +0400

    fixed

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -100,8 +100,8 @@ function classNames(...classes) {
 
 export default function Header() {
   return (
-    <div className="relative bg-hero bg-cover h-full  bg-gray-50 rom-slate-600">
-      <Popover className="stiky top-0 z-50 bg-white shadow">
+    <div className="relative bg-hero bg-cover h-full bg-gray-50 rom-slate-600">
+      <Popover className="fixed w-full top-0 z-50 bg-white shadow">
         <div className="max-w-7xl mx-auto px-4 sm:px-6">
           <div className="flex justify-between items-center py-6 md:justify-start md:space-x-10">
             <div className="flex justify-start lg:w-0 lg:flex-1">

77280cbf3e776236b4582a07c79f6e2c68e13b83
omari <omari.jalagania@gmail.com>
2022-01-13 21:34:41 +0400

    changes

--- components/Header/DroneLabel.js
+++ components/Header/DroneLabel.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Image from "next/image";
 
 function DroneLabel() {
   return (
     <section className="h-screen bg-gray-100 p-5">
-      <div className="flex items-center justify-around">
-        <div className="h-screen w-1/2 text-center animate__animated animate__slideInLeft">
+      <div className="flex items-center justify-between sm:flex-col lg:grid lg:grid-cols-2">
+        <div className="text-center animate__animated animate__slideInLeft">
           <p className="p-7">მარკირების ინსტრუქცია</p>
           <p className="p-7">
             ექსპლუატაციის დაწყებამდე აუცილებელია უპილოტო საჰაერო ხომალდის
@@ -22,7 +23,12 @@ function DroneLabel() {
         </div>
 
         <div className="w-96 h-90 animate__animated animate__slideInRight">
-          <img src="../img/drone-label.jpeg" alt="drone label" />
+          <Image
+            width={400}
+            height={300}
+            src="/img/drone-label.jpeg"
+            alt="drone label"
+          />
         </div>
       </div>
     </section>
--- components/Header/Header.js
+++ components/Header/Header.js
@@ -124,7 +124,7 @@ export default function Header() {
                 <span className="sr-only">Workflow</span>
                 <img
                   className="h-8 w-auto sm:h-10"
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                  src="../img/logo.png"
                   alt=""
                 />
               </a>
